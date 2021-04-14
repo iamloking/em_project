57061f3cb1a4d09d8823e18ec07b2a2a06f36633
Lokesh Jain <51840796+iamloking@users.noreply.github.com>
2021-04-14 12:44:57 +0530

    Update 1
    Detailed comments to make the code more understandable

--- v2.py
+++ v2.py
@@ -2,8 +2,11 @@
 import tkinter as tk
 from tkinter import *
 
+
 class MainWindow:
+    #The first method that gets run when MainWindow class in instantiated
     def __init__(self):
+        #Settig up the main window, and fixing the geometry
         self.root = Tk()
         self.root.title("Machine's Project")
         self.root.geometry("500x500")
@@ -12,24 +15,15 @@ class MainWindow:
         self.root.resizable(True, True)
         self.root.configure(bg="black")
         
+        #This function will run the method, that builds the first window outlook in our main window
         self.window_one()
-
-    def take_input(self):
-        self.input = "Parameter1 = " + self.input_param_1.get("1.0", "end-1c")
-        self.input = self.input+"\n"+"Parameter2 = "+ self.input_param_2.get("1.0", "end-1c")
-
-        self.Output.insert(END,self.input)
-        self.save_param_values()
-        print(self.param1,self.param2)
-
-    def save_param_values(self):
-        self.param1 = self.input_param_1.get("1.0", "end-1c")
-        self.param2 = self.input_param_2.get("1.0", "end-1c")
-
         
-
     def window_one(self):
+        #The next command will clean the window if there are any widgets present
         self.clean_window(self.root)
+        
+        #Creating the layout of the window
+        
         #Title of the window
         self.a = Label(self.root, text ="Please enter the following parameters",fg='white',font=20) 
         self.a.pack(padx=7,pady=7)
@@ -63,7 +57,7 @@ class MainWindow:
         self.Display = Button(self.frame1, height = 2,
                         width = 20, 
                         text ="Show",fg="white",
-                        command = lambda:self.take_input())
+                        command = lambda:self.take_input())  #The take input method shows the entered parameters in the output box
         self.Display.configure(bg="purple")
         self.frame1.configure(bg="black")
 
@@ -86,10 +80,10 @@ class MainWindow:
         self.next_button = Button(self.frame3,height = 2,
                         width = 20, 
                         text ="Next",fg="white",
-                        command = lambda:self.window_two())
+                        command = lambda:self.window_two()) #This button will clear destroy all the widgets of window one  and create the layout for window two.
         self.next_button.configure(bg="purple")        
 
-        
+        #Setting the geometry of different widgets
         self.param_1_label.grid(row=0,column=0,pady=10)
         self.input_param_1.grid(row=0,column=1,pady=10)
         self.param_2_label.grid(row=1,column=0,pady=10)
@@ -102,10 +96,30 @@ class MainWindow:
         self.Output.grid(row=0,column=1,pady=10)
         self.next_button.pack(padx=7,pady=7)
         self.root.mainloop()
+    
+    #The method below shows the entered parameters in the output text box
+    def take_input(self):
+        #The display message
+        self.input = "Parameter1 = " + self.input_param_1.get("1.0", "end-1c")
+        self.input = self.input+"\n"+"Parameter2 = "+ self.input_param_2.get("1.0", "end-1c")
+        #Function displays the message
+        self.Output.insert(END,self.input)
+        
+        #The diff parameters will be stored safely by  the method below
+        self.save_param_values()
+        #print(self.param1,self.param2)
+
+    def save_param_values(self):
+        self.param1 = self.input_param_1.get("1.0", "end-1c")
+        self.param2 = self.input_param_2.get("1.0", "end-1c")
+
+        
+
+    
 
     
         
-        
+    #cleans the window of any widgets
     def clean_window(self,wid):
         self. widget_list = wid.winfo_children()
         for item in self.widget_list :
@@ -114,10 +128,12 @@ class MainWindow:
         
         for widget in self.widget_list:
             widget.destroy()
-
+    
+    
+    #Backend and Gui code for motor will be written here
     def motor_output(self):
         pass
-
+    #Backend and Gui code for generator will be written here
     def generator_output(self):
         pass
 
